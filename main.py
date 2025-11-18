493bb3cc191f2aa1c093c5a03fe1903b3fdf9595
EdvinoT <edvinot82@gmail.com>
2025-11-18 14:20:21 +0000

    commit

---
+++ main.py
@@ -0,0 +1,11 @@
+first_n = int(input("What is the first number " ))
+second_n = int(input("What is the second number " ))
+
+if first_n > second_n:
+    print ("Success")
+
+elif second_n > first_n:
+    print("Failure")
+
+elif first_n == second_n:
+    print("Numbers Equal!")
