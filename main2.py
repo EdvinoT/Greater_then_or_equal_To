7e8d7c59d8d2c888531258564bd4aa57d9ec699e
EdvinoT <edvinot82@gmail.com>
2025-11-19 14:19:58 +0000

    commit

---
+++ main2.py
@@ -0,0 +1,10 @@
+def sum():
+    sum_list=[1,6,4,8,10,-4,-7.4,6,-6,]
+    posi_n=[]
+    for number in sum_list:
+        if number > 0:
+            posi_n.append(number)
+        print(posi_n)
+    
+    return sum()
+
